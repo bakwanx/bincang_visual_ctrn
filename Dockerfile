38a1e16eb3fe135848df682cfee86d8183519ec2
farhanfadhilah <farhanfadhilah87@gmail.com>
2025-05-27 22:45:17 +0700

    turnserfer configuration

--- Dockerfile
+++
@@ -1,8 +0,0 @@
-FROM instrumentisto/coturn:latest
-
-COPY turnserver.conf /etc/coturn/turnserver.conf
-COPY entrypoint.sh /entrypoint.sh
-
-RUN chmod +x /entrypoint.sh
-
-ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# Dynamically append user from env
-if [ -n "$TURN_USER" ] && [ -n "$TURN_PASS" ]; then
-  echo "user=$TURN_USER:$TURN_PASS" >> /etc/coturn/turnserver.conf
-fi
-
-# Start coturn
-exec turnserver -c /etc/coturn/turnserver.conf
